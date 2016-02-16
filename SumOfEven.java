01e7a25bee4861d39825b85b9d6c5a5e45e15117
michaelukweb <michaelafc01@gmail.com>
2016-02-16 15:57:02 +0000

    Adding new Java Maths Program

---
+++ SumOfEven.java
@@ -0,0 +1,22 @@
+
+public class SumOfEven {
+
+    public static void main (String [] args) {
+    
+    
+        
+        int sum = 0;
+        
+        for (int i = 0; i <= 100; i++) {
+
+            if (i % 2 == 0){
+            
+                sum += i;
+                System.out.println("The value of i is: " + i);
+            } 
+        }
+        
+        System.out.println("The total is "+ sum);
+    
+    }
+}
